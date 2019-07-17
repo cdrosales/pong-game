a9b9a420cdc5e60780eb96042f3efbe8b45ae52f
cdrosales <cara10denise@hotmail.com>
2019-07-17 10:11:19 -0400

    feat: added in svg in canvas

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,25 @@
+import {SVG_NS} from '../settings' //dont need settings
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+		this.gameElement = document.getElementById(this.element) // caling div id = "game" from HTML
   }
 
-  render() {
-		// More code goes here....
+  render() { 
+
+    // clear board 
+    this.gameElement.innerHTML = '' // so canvas doesnt stack on top of each other because of the infinite loop
+   // create SVG element for the board
+
+   let svg = document.createElementNS(SVG_NS, 'svg')
+   svg.setAttributeNS(null, 'width', this.width) // accepts one more parameter than the reg div.setAttribute
+   svg.setAttributeNS(null, 'height', this.height) //this is referring to index.js
+   svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`) // 0-width 0-height || `` let you add variables without +  
+   this.gameElement.appendChild(svg) //appends to html
+
   }
 }
