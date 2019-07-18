1dd60290c69fab9d043852653c5114ed9875f7bd
cdrosales <cara10denise@hotmail.com>
2019-07-18 14:38:46 -0400

    feat: added the scoreboard with functionality

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -2,7 +2,7 @@ import {SVG_NS, KEYS} from '../settings' //dont need settings
 import Board from './Board'
 import Paddle from './Paddle'
 import Ball from './Ball'
-
+import Score from './Score'
 
 export default class Game {
   constructor(element, width, height) {
@@ -43,6 +43,10 @@ export default class Game {
       KEYS.down
     )
 
+    this.score1 = new Score(this.width /2 - 50, 30, 30) // score positioning 
+    this.score2 = new Score(this.width /2 + 50, 30, 30)
+        
+
       // event listener to pause game 
       document.addEventListener('keydown', event => {
         switch(event.key) {
@@ -79,5 +83,10 @@ export default class Game {
    this.player1.render(svg)
    this.player2.render(svg)
    this.ball.render(svg, this.player1, this.player2)
+
+
+   this.score1.render(svg, this.player1.score)
+   this.score2.render(svg, this.player2.score)
+
   }
 }
